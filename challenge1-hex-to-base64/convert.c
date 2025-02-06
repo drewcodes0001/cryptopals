b7475f6f08cc94d3382e4dadcb786a0110d6e387
drewcodes0001 <andrew.tang367@gmail.com>
2025-02-06 13:43:49 -0500

    Working on convert.c and added gitignore for executables

--- challenge1-hex-to-base64/convert.c
+++ challenge1-hex-to-base64/convert.c
@@ -1,5 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc, char **argv) {
+void convert(FILE *fp) {
     
+}
+
+int main (int argc, char **argv) {
+    if (argc == 2) {
+        FILE *fp = fopen(*(argv + 1), "rb");
+        if (fp != NULL) convert(fp);
+        else exit(EXIT_FAILURE);
+    } else if (argc > 3) {
+        fprintf(stderr, "Passing in more than one file is not handled\n");
+        exit(EXIT_FAILURE);
+    } else {
+        convert(stdin); 
+    }
 }
